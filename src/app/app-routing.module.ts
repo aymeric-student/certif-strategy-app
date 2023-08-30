144a3d85a652910e2d63edfccec31e3cdb1aa593
aymeric.maillot@efrei.net <66923802+Votybe@users.noreply.github.com>
2023-08-30 21:01:25 +0200

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+/*
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+*/
+
+import { Route } from '@angular/router';
+
+export const appRoutes: Route[] = [
+/*  {
+    path: '',
+    loadChildren: () => import('a').then(m => m.HomeModule),
+  },*/
+];
